ca615bbb95a6babc73a186e60211142614c6ad2f
jaedmunt <jaedonmuntonwork@gmail.com>
2024-10-18 22:35:29 +0100

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import { ChakraProvider, Box, Heading, Text } from '@chakra-ui/react';
 import Pricing from '@/components/ui/Pricing/Pricing';
 import { createClient } from '@/utils/supabase/server';
 import {
@@ -16,17 +15,10 @@ export default async function PricingPage() {
   ]);
 
   return (
-    <ChakraProvider>
-      <Box p={4}>
-        <Heading as="h1" size="xl" mb={4}>
-          Pricing Page
-        </Heading>
-        <Pricing
-          user={user}
-          products={products ?? []}
-          subscription={subscription}
-        />
-      </Box>
-    </ChakraProvider>
+    <Pricing
+      user={user}
+      products={products ?? []}
+      subscription={subscription}
+    />
   );
 }
